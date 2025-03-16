c8d93df4334bcea4f6fbd46b38c2ac750653cb91
Niami <4783533-Niami@users.noreply.gitlab.com>
2025-03-16 18:23:08 +0000

    started user login and registration

--- Login/login.js
+++ Login/login.js
@@ -1,15 +1,93 @@
-function login() {
-    let username = document.getElementById("Username").value
-    let password = document.getElementById("Password").value
-
-    
-
-
-    // Get result of login somehow
-
-
+// TODO: clean sql to prevent injection
+function CleanSQL(Input) {
+    return Input
     
     
+}
 
+window.addEventListener('load', () => {
+    document.getElementById('Login').addEventListener('click', () => {
+        Login(document.getElementById("Username").value, document.getElementById("Password").value)  
+    })
+})
 
+async function Login(Username, Password) {
+    Username = CleanSQL(Username)
+    Password = CleanSQL(Password)
+
+    const LoginQuery = `SELECT Username FROM Users WHERE Username = "${Username}" AND Password = "${Password}";`
+    executeDatabaseQuery(LoginQuery).then((result) => {
+        if (result.data.length > 0) {
+            return true
+        } else {
+            return false
+        }
+    })
+
+    
+}
+
+const DatabaseConnectionData = {
+    url: 'https://phanisek01.webhosting1.eeecs.qub.ac.uk/dbConnector.php',
+    hostname: "localhost",
+    username: "phanisek01", // ! Enter own username
+    password: "nDKM7BtMSYYxWc9F",// ! Enter own password
+    database: "phanisek01", // ! Change to group DB when uploading
+}
+
+/**
+ * Sends a SQL query to the database defined at the start of the file
+ * @param {string} SQLQuery 
+ * @returns Promise. Successful if SQL query succeeds, Rejects if there is error at any part of the request
+ */
+async function executeDatabaseQuery(SQLQuery) {
+    let params = new URLSearchParams();
+    params.append('hostname', DatabaseConnectionData.hostname);
+    params.append('username', DatabaseConnectionData.username);
+    params.append('password', DatabaseConnectionData.password);
+    params.append('database', DatabaseConnectionData.database);
+    params.append('query', SQLQuery);
+
+    const DBPromise = new Promise((resolve, reject) => {
+        try {
+            let fetchPromise = fetch(DatabaseConnectionData.url, {
+                method: 'POST',
+                body: params
+            }); 
+    
+            fetchPromise.then((result) => {
+                result.json().then((jsonResult) => {
+                    if (jsonResult.error) {
+                        console.log(jsonResult.error.toString());
+                        // Passes result directly to not complicate handling result
+                        reject(jsonResult)
+                    }
+                    else if (jsonResult.data) {
+                        // Passes result directly to not complicate handling result
+                        resolve(jsonResult)
+                    }
+                    else {
+                        if (jsonResult.affected_rows !== undefined) {
+                            resolve({
+                                success: jsonResult.success,
+                                affected_rows: jsonResult.affected_rows
+                            })
+                        } else {   
+                            resolve({
+                                success: jsonResult.success,
+                            })
+                        }
+                    }
+                }) 
+            }) 
+        } catch (error) {
+            console.error('Error parsing JSON:', error);
+            reject({
+                error: true,
+                errorReason: error
+            })
+        }
+    })
+
+    return DBPromise
 }
